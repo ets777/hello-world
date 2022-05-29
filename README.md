2cd806d36864aac5c812298f07cdd7a2066c5705
ETS <dmitry.klishev@gmail.com>
2022-05-29 12:32:09 +1000

    new font

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ First javascript-project on github. App shows text 'Hello World!'.
 
 ### Versions history
 
+- 1.2.0 - new font
 - 1.1.1 - `readme.md` fixed
 - 1.1.0 - text is displayed on the canvas
 - 1.0.0 - first version, text is displayed by `document.write()`
--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const app = document.getElementById('app');
 app.width = 600;
 app.height = 300;
 const context = app.getContext('2d');
+
+const font = new FontFace('fifaks', 'url(font/fifaks.woff2)');
 context.textAlign = 'center';
-context.font = '48px arial';
-context.fillText('Hello World!', app.width / 2, app.height / 2);
+
+font.load().then(function () {
+    context.font = '48px fifaks';
+    context.fillText('Hello World!', app.width / 2, app.height / 2);
+});
